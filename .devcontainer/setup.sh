31594b2e2f613bd6af831a5f82bb87daeba7d973
Brady <bradyg@microsoft.com>
2020-03-31 09:26:13 +0000

    core 3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,7 +4,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
-sudo apt-get install azure-functions-core-tools
+sudo apt-get install azure-functions-core-tools-3
 curl https://cdn.porter.sh/canary/install-linux.sh | bash
 export PATH=~/.porter/:$PATH
 porter mixins install jq --url https://github.com/squillace/porter-jq/releases/download --version v0.1.0
