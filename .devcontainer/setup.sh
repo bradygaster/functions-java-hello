f9047bc7661ff6bdc9aa1a59c68abbe8adbcba99
Brady <bradyg@microsoft.com>
2020-03-31 09:16:14 +0000

    added functions tools to startup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install jq maven
+sudo apt-get -y install jq maven azure-functions-core-tools-3
 curl https://cdn.porter.sh/canary/install-linux.sh | bash
 export PATH=~/.porter/:$PATH
 porter mixins install jq --url https://github.com/squillace/porter-jq/releases/download --version v0.1.0
